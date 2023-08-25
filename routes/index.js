d15f18b21d40aa2449a6066c5cdcbf761b6adb39
romzy234 <cyrilogoh@gmail.com>
2023-08-25 11:53:53 +0100

    Added Routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const {} = require('../controller/index')
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/detail', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/webhook/url', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
