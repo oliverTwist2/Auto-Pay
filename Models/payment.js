87fc79c3585cab48d9492150cf1d4f9ecc8ba552
Simplystina <62130713+Simplystina@users.noreply.github.com>
2023-08-25 02:38:58 +0100

    added models

---
+++ Models/payment.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const paymentSchema = new mongoose.Schema(
+    {
+      
+        ticket_id: { type: String, required: true },
+        reference_no: { type: String, required: true },
+        status: { type: Number, required: true }
+    },
+    {
+        timestamps: true, toJSON: { virtuals: true }
+    }
+)
+
+const paymentModel = mongoose.model('payments', paymentSchema)
+
+module.exports = paymentModel
---
+++ Models/ticket.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const ticketSchema = new mongoose.Schema(
+    {
+    email: {type:String, required: true},
+    recepit_id : {type:String, required: true},
+    concert_id: {type:String, required: true},
+    amount:{type:String, required:true},
+    ticket_count: { type: Number, required: true },
+    no_sold: { type: Number, required: true }
+  },
+    {
+        timestamps: true, toJSON: { virtuals: true }
+    }
+)
+
+const ticketModel = mongoose.model('tickets', ticketSchema)
+
+module.exports = ticketModel
