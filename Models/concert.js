2db88ca13348797ed8a87ea072474911d44bab4e
romzy234 <cyrilogoh@gmail.com>
2023-08-25 11:54:27 +0100

    Fix In Model

--- Models/concert.js
+++ Models/concert.js
@@ -5,13 +5,14 @@ const concertSchema = new mongoose.Schema(
          name: { type: String, required: true },
         artist: { type: String, required: true },
         venue: { type: String, required: true },
-        no_sold: { type: Number, required: true }
+        no_sold: { type: Number, required: true },
+        amount: { type: Number, required: true }
     },
     {
         timestamps: true, toJSON: { virtuals: true }
     }
 )
 
-const ticketModel = mongoose.model('tickets', ticketSchema)
+const concertModel = mongoose.model('concerts', concertSchema)
 
-module.exports = ticketModel
+module.exports = concertModel
