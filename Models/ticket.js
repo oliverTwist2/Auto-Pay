4ceac3da4562e8bcbfb219912ff9fa30d78f4abc
Simplystina <62130713+Simplystina@users.noreply.github.com>
2023-08-26 10:09:49 +0100

    implemented the endpoint for verifying payment

--- Models/ticket.js
+++ Models/ticket.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose')
 const ticketSchema = new mongoose.Schema(
     { 
     email: {type:String, required: true},
-    recepit_id : {type:String, required: true},
-    amount:{type:String, required:true},
-    ticket_count: { type: Number, required: true },
-    no_sold: { type: Number, required: true }
+    amount_paid:{type:Number},
+    ticket_count: { type: Number, required: true , default:0},
+    no_sold: { type: Number , default: 0},
+    reference_no: { type: String, required: true },
+    access_code: { type: String, required: true },
+    payment_status: { type: String, required: true , default: false}
   },
     {
         timestamps: true, toJSON: { virtuals: true }
--- controller/index.js
+++ controller/index.js
@@ -4,10 +4,11 @@ const { sendMail } = require("../mail");
 
 const Ticket = require("../Models/ticket");
 //const Concert = require("../Models/concert");
-const Payment = require("../Models/payment");
+
 const axios = require("axios")
 const dotenv = require("dotenv")
 const crypto = require('crypto');
+const ticketModel = require("../Models/ticket");
 dotenv.config()
 /**
  * @author GUEST <******@gmail.com>
@@ -16,40 +17,7 @@ dotenv.config()
  * @access Public
  * @type POST
  */
-exports.createTicket = asyncHandler(async (req, res, next) => {
-  const { email, quantity, concert, name } = req.body;
-  if (!email) {
-    return next(new ErrorResponse("Email Is Required", 400));
-  }
 
-  if (!quantity) {
-    return next(new ErrorResponse("Missing Entry, quantity is required", 400));
-  }
-  if (!concert) {
-    return next(
-      new ErrorResponse("Missing Entry, concert ID is required", 400)
-    );
-  }
-  const isValid = await Concert.findOne({_id:concert});
-
-  if (!isValid) {
-    return next(
-      new ErrorResponse("Concert Not Found", 404)
-    );
-  }
-
-  if (isNaN(quantity)) {
-    return next(new ErrorResponse("Invaild Data Type", 400));
-  }
-
-  let amount =  isValid.amount * quantity
-  const data = await Ticket.create({ email, quantity, concert, name, amount });
-
-  res.status(200).json({
-    success: true,
-    data: data,
-  });
-});
 
 
 
@@ -57,40 +25,64 @@ exports.createTicket = asyncHandler(async (req, res, next) => {
 
 exports.initializePayments = asyncHandler(async(req,res,next)=>{
 
-  const {email, amount} = req.body
-  url = "https://api.paystack.co/transaction/initialize"
+      const {email, quantity} = req.body
 
-  const headers = {
-  Authorization: `Bearer ${process.env.SECRET_KEY}`,
-  "Content-Type": "application/json"
- }
+      if(!(email && quantity)){
+        return next(new ErrorResponse('Incomplete details', 400));
+
+      }
+        url = "https://api.paystack.co/transaction/initialize"
+
+        const headers = {
+        Authorization: `Bearer ${process.env.SECRET_KEY}`,
+        "Content-Type": "application/json"
+      }
   
-  data = { 
-  email: email,
-    amount : amount
-  }
+      data = { 
+        email: email,
+        amount : 2000
+      }
   try {
     const resp = await axios.post(url,data,{headers})
     console.log(resp.data,"resp")
-    res.status(200).json({message:'successful', data:resp?.data})
+
+   // create tickets
+    const ticket = await ticketModel.create({
+      email: email,
+      ticket_count: quantity,
+      reference_no: resp.data?.data?.reference,
+      access_code: resp.data?.data?.access_code
+    })
+    res.status(200).json({message:'Authorization URL created', data:resp?.data})
   } catch (error) {
-    console.log(error,"error")
+    next(error)
+    //console.log(error, "error")
   }
 })
 
 exports.verifyPayment = asyncHandler(async (req,res,next)=>{
 
-  console.log(req.body,"reqqqqqqq")
       const hash = crypto.createHmac('sha512', process.env.SECRET_KEY).update(JSON.stringify(req.body)).digest('hex');
-      if (hash == req.headers['x-paystack-signature']) {
-        // Retrieve the request's body
-        const event = req.body;
-        // Do something with event  
-        console.log(event)
+      if (hash != req.headers['x-paystack-signature']) {
+        return
       }
+      // Retrieve the request's body
+      const data = req.body;
   try {
-   
+    if (data.event === "charge.success") {
+      const ticket = await ticketModel.find({ reference_no: data?.data?.reference })
+      const updateTicket = await ticketModel.updateOne(
+        { reference_no: data?.data?.reference },
+        { $set: { amount: data?.data?.amount, payment_status: true } }
+      )
+     
+      const updateNoSold = await ticketModel.updateMany({}, { $set: { no_sold: ticket[0]?.no_sold + ticket[0]?.ticket_count } })
+      
+      return res.status(200).json({ message: "Successful payment" })
+    }
+    return res.status(200).json({ message: "Unsuccessful payment" })
   } catch (error) {
+    next(error)
     console.log(error,"error")
   }
 })
